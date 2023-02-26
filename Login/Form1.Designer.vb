7e0d508acdad4c974aca1dde6229e5ed8b1d56fd
ardianza <126378771+ardianza@users.noreply.github.com>
2023-02-26 14:25:58 +0700

    Add files via upload

---
+++ Login/Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.lblError = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(113, 46)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(144, 20)
+        Me.txtUsername.TabIndex = 0
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(113, 82)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(144, 20)
+        Me.txtPassword.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(28, 49)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Username"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(28, 85)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Password"
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Location = New System.Drawing.Point(182, 127)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(75, 23)
+        Me.btnLogin.TabIndex = 4
+        Me.btnLogin.Text = "Login"
+        Me.btnLogin.UseVisualStyleBackColor = True
+        '
+        'lblError
+        '
+        Me.lblError.AutoSize = True
+        Me.lblError.ForeColor = System.Drawing.Color.Red
+        Me.lblError.Location = New System.Drawing.Point(28, 20)
+        Me.lblError.Name = "lblError"
+        Me.lblError.Size = New System.Drawing.Size(0, 13)
+        Me.lblError.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(290, 175)
+        Me.Controls.Add(Me.lblError)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Name = "Form1"
+        Me.Text = "Form Login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtUsername As System.Windows.Forms.TextBox
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btnLogin As System.Windows.Forms.Button
+    Friend WithEvents lblError As System.Windows.Forms.Label
+
+End Class
---
+++ Login/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+
+    Private Sub btnLogin_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnLogin.Click
+        'Membuat koneksi database
+        Dim conn As New OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\data\users.accdb")
+
+        'Membuka koneksi ke database
+        conn.Open()
+
+        'Command untuk memeriksa data login
+        Dim cmd As New OleDb.OleDbCommand("SELECT * FROM users WHERE username = @username AND password = @password", conn)
+
+        'Mengisi Parameter pada command
+        cmd.Parameters.AddWithValue("@username", txtUsername.Text)
+        cmd.Parameters.AddWithValue("@password", txtPassword.Text)
+
+        'Menjalankan command dan menyimpan hasil nya dalam data reader
+        Dim dr As OleDb.OleDbDataReader = cmd.ExecuteReader()
+
+        'Cek data di database
+        If dr.Read() Then
+            'Jika username dan password ada di database
+            MessageBox.Show("Login Berhasil")
+        Else
+            'Jika username dan password salah atau tidak ada si dtabase
+            lblError.Text = "Username atau Password salah"
+        End If
+
+        'Menutup koneksi ke Database
+        dr.Close()
+        cmd.Dispose()
+        conn.Close()
+
+    End Sub
+End Class
